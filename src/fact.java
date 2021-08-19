3530fcc98dcd6b1f254a8038a186ccf364040a0d
nehasangwai <nehasangwai354@gmail.com>
2021-08-19 16:26:41 +0530

    First Commit

---
+++ src/fact.java
@@ -0,0 +1,8 @@
+import com.factorial.CalculateFactorial;
+
+public class fact {
+	public static void main(String[] args) {
+		CalculateFactorial c = new CalculateFactorial();
+		System.out.println(c.calculateFactorial(4));
+	}
+}
